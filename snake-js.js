210d28d5ac250c85c0a86e6bfe3a30d7f451adfa
rekin5320 <mmach331@gmail.com>
2022-06-06 22:36:36 +0200

    Function allowing to access individual tile

--- snake-js.js
+++ snake-js.js
@@ -1,4 +1,16 @@
+const board = document.getElementById("board").children[0]
+
+function get_tile(x, y) {
+    return board.children[y].children[x]
+}
+
 function something() {
-    board = document.getElementById("board").children[0]
-    board.children[1].children[2].style.background = "red"
+    let tile = get_tile(2, 1)
+
+    if (tile.style.background === "gray") {
+        tile.style.background = "red"
+    }
+    else {
+        tile.style.background = "gray"
+    }
 }
