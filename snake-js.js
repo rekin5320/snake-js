cc05433db9606db917a45a3e21e3569ea6fb8ba6
rekin5320 <mmach331@gmail.com>
2022-06-06 23:18:49 +0200

    You can move red tile

--- snake-js.js
+++ snake-js.js
@@ -1,16 +1,42 @@
-const board = document.getElementById("board").children[0]
+function check_key(e) {
+    switch_tile_color(xx, yy)
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("up")
+            yy -= 1
+            break;
+        case "ArrowDown":
+            console.log("down")
+            yy += 1
+            break;
+        case "ArrowLeft":
+            console.log("left")
+            xx -= 1
+            break;
+        case "ArrowRight":
+            console.log("right")
+            xx += 1
+    }
+    switch_tile_color(xx, yy)
+}
 
 function get_tile(x, y) {
     return board.children[y].children[x]
 }
 
-function something() {
-    let tile = get_tile(2, 1)
+function switch_tile_color(x, y) {
+    let tile = get_tile(x, y)
 
-    if (tile.style.background === "gray") {
-        tile.style.background = "red"
+    if (tile.style.backgroundColor === "red") {
+        tile.style.backgroundColor = "gray"
     }
     else {
-        tile.style.background = "gray"
+        tile.style.backgroundColor = "red"
     }
 }
+
+document.onkeydown = check_key
+const board = document.getElementById("board").children[0]
+
+var xx = 0, yy = 0
+switch_tile_color(xx, yy)
